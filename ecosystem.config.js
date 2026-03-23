1a620f80888211fc31f73e49b675b8be9577f1d3
ivanamx <ivan@ironslash.com>
2026-03-23 14:42:09 -0600

    frontend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,21 @@
-module.exports={apps:[{name:"memoriales-bot",script:"./dist/index.js",instances:1,max_memory_restart:"500M",env:{NODE_ENV:"production"},error_file:"./logs/error.log",out_file:"./logs/out.log",autorestart:true}]};
+module.exports = {
+  apps: [
+    {
+      name: "memoriales-bot",
+      cwd: "/home/ivanam/projects/memorial",
+      script: "./dist/index.js",
+      instances: 1,
+      exec_mode: "fork",
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "production",
+        PORT: 8780,
+        PUBLIC_URL: "http://memorialescelestiales.com"
+      },
+      env_file: "/home/ivanam/projects/memorial/.env",
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
+      autorestart: true
+    }
+  ]
+};
